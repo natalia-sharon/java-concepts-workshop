9f878b3ec32bdd713283d123528ec7ccb1e4dcf0
natalia-sharon <natalia.sharon@turntabl.io>
2021-10-26 19:34:18 +0100

    Initial commit for overloading and overriding workshops

---
+++ overriding/src/Football.java
@@ -0,0 +1,10 @@
+/*
+ * Welcome to your method overriding workshop!
+ * Please make your changes in this class.
+ * Use function overriding to print out the name and number of players from the Main class.
+ */
+
+public class Football extends Sports {
+
+}
+
---
+++ overriding/src/Sports.java
@@ -0,0 +1,15 @@
+/*
+ * Welcome to your method overriding workshop!
+ * Please don't touch this class. Make your changes in the Football class.
+ * Use function overriding to get the Football class to print out the name and number of players from the Main class.
+ */
+public class Sports {
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName());
+    }
+}
