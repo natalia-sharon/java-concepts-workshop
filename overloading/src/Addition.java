5554877028be2b7e779bc505e25cc567396cabe4
MaryKwawu <you@example.com>
2021-11-03 16:21:58 +0000

    overridding and overloading lab

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -5,6 +5,22 @@
  */
 
 public class Addition {
+        public int add(int a, int b, int c) {
+            int sum = a + b +c;
+            return sum;
+        }
 
-}
+        public int add(int a, int b) {
+            int sum = a + b ;
+            return sum;
+        }
+
+        public float add(int a, int b, float c) {
+            float sum = a + b ;
+            return sum;
+        }
+    }
+
+
+
 
