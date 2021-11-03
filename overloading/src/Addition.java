105bb3b1aed6d7de46c50e931b6323a624bf7f97
Godric <godric.dolphine@turntabl.io>
2021-11-03 14:39:39 +0000

    Tutorial challenge answers

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -4,7 +4,16 @@
  * Use function overloading to implement TWO methods which prints out the sum of the inputs.
  */
 
-public class Addition {
+import java.util.Arrays;
 
+public class Addition {
+    public void add(int ...a) {
+        System.out.println(Arrays.stream(a).sum());
+    }
+
+
+    public void add(int a, int b, float c) {
+        System.out.println(a + b + c);
+    }
 }
 
