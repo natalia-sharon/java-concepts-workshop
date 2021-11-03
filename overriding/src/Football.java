ecb58b97b5209224c72aa3e609fa8d9961f8a57b
dedol1 <afoteylaryeasamuel@gmail.com>
2021-11-03 15:18:56 +0000

    Overriding methods

--- overriding/src/Football.java
+++ overriding/src/Football.java
@@ -6,5 +6,23 @@
 
 public class Football extends Sports {
 
+    private int playerNumber;
+    private String playerName;
+
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println("The player's name is " + getName() + " and player's number is " + getPlayerNumber());
+
+    }
 }
 
