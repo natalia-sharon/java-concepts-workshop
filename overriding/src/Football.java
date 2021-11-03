db1914fc820f58d42abbf97e26ef56a9e4c623e6
Kingsley Abru <44739481+kingsabru@users.noreply.github.com>
2021-11-03 16:00:41 +0000

    Feature/kingsley abru (#1)
    * Solved challenge
    
    * Rebuilt project to fix error

--- overriding/src/Football.java
+++ overriding/src/Football.java
@@ -4,7 +4,23 @@
  * Use function overriding to print out the name and number of players from the Main class.
  */
 
-public class Football extends Sports {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Football extends Sports {
+    protected List<Team> teams = new ArrayList<>();
+
+    public void addTeam(Team team){
+        teams.add(team);
+    }
+
+    public void printPlayersDetails(){
+        teams.forEach(
+                t -> t.getPlayers().forEach( p ->
+                        System.out.printf("No.: %d Name: %s\n", p.getNumber(), p.getName()
+                        )
+                )
+        );
+    }
 }
 
