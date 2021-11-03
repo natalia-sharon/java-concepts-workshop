6ced64478ac82d0f4ab735691e2650d7cd12ce36
Adiba Aweperi Emmanuel <eabaagah@gmail.com>
2021-11-03 14:42:02 +0000

    method overlaoding

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -6,5 +6,16 @@
 
 public class Addition {
 
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public float add(int x, int y, float z) {
+        return x + y + z;
+    }
+
+    public int add(String x, String y) {
+        return Integer.parseInt(x) + Integer.parseInt(y);
+    }
 }
 
