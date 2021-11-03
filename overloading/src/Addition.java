4a4f70211896abeb4a6e06fd111af2c0c5b77aef
dev-navian <ivan.annan@turntabl.io>
2021-11-03 15:04:52 +0000

    overloading the add method

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -6,5 +6,21 @@
 
 public class Addition {
 
+    public int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public int add(int numOne, int numTwo, int numThree) {
+        return numOne + numTwo + numThree;
+    }
+
+    public float add(int numOne, int numTwo, float numThree) {
+        return (float) (numOne + numTwo + numThree);
+    }
+
+    public int add(String numOne, String numTwo) {
+        return Integer.parseInt(numOne) + Integer.parseInt(numTwo);
+    }
+
 }
 
